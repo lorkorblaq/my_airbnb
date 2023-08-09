eb79e342bb3ad230e7a0258c3a651cff96199b50
blaq <f3mioloko@gmail.com>
2023-08-10 00:41:36 +0100

    folder update

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,7 @@
-from base_model import Calculations
+#!/usr/bin/python3
+"""__init__ magic method for models directory"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
