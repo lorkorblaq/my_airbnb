f338010a8eff57ba52e8a417d7ee32fb8b3896ca
blaq <f3mioloko@gmail.com>
2023-08-10 19:33:33 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -14,23 +14,20 @@ class BaseModel:
             *arguments args (any): Unused.
             **keyword arguments kwargs (dict): Key/value pairs of attributes.
         """
-        print(args)
-        for key, value in kwargs.items():
-            print(value)
-        # tform = "%Y-%m-%dT%H:%M:%S.%f"
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        
-        # if len(kwargs) != 0:
-            # for k, v in kwargs.items():
-                # if k == "created_at" or k == "updated_at":
-                    # self.__dict__[k] = datetime.strptime(v, tform)
-                # else:
-                    # self.__dict__[k] = v
-        # else:
-            # models.storage.new(self)
 
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+             models.storage.new(self)
+BaseModel()
     # def save(self):
         # """Update updated_at with the current datetime."""
         # self.updated_at = datetime.today()
